b9f6b429e7ebd16938e5f700b70d853c1a5355d2
Floony7 <fred.lunjevich@stretchsense.com>
2025-04-17 07:24:58 +0100

    User Controller routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Patch, Delete, Get, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
@@ -9,4 +9,21 @@ export class UsersController {
         const {email, password} = body;
         this.UsersService.create(email, password);
     };
+
+    @Get(':id')
+    findUser(@Param('id') id: string) {
+        const userId = parseInt(id);
+        return this.UsersService.findOne(userId);
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.UsersService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        const userId = parseInt(id);
+        return this.UsersService.remove(userId)
+    }
 }
